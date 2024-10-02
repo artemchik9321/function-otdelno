2b27ab26dddcabe47ab5402828cef53251512b23
artemchik9321 <artemtatsin9321@gmail.com>
2024-10-02 23:55:48 +0300

    Initial commit

---
+++ func.js
@@ -0,0 +1,35 @@
+ function twonumbers(n,m) 
+ {
+     if(n%2==0 && m%2==0) 
+     {
+         return n*m;
+     }
+     else if(n%2!=0 && m%2!=0) 
+     {
+         return n+m;
+     }
+     else if(n%2!=0 && m%2==0) 
+     {
+         return n;
+     }
+     else if(n%2==0 && m%2!=0) 
+     {
+         return m;
+     }
+ }
+ n = parseInt(prompt("enter number "));
+ m = parseInt(prompt("enter number "))
+ document.write("number is "+twonumbers(n,m));
+
+function Rectangle(x1, x2, x3, x4, x5, x6, x7, x8) 
+{
+    if (x1 == x7 && x2 == x4 && x3 == x5 && x6 == x8) 
+    {
+        document.write("Rectangle can exist!!");
+    }
+    else 
+    {
+        document.write("Your rectangle can`t exist ")
+    }
+}
+Rectangle(2, 4, 5, 4, 5, 1, 2, 1)
